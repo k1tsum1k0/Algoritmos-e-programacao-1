39597a59713a726aad7ff6f5ba0e0638bde9bae9
k1tsum1k0 <140666056+k1tsum1k0@users.noreply.github.com>
2025-03-18 20:14:43 -0400

    Add files via upload

---
+++ semana1/exlivrocap2.py
@@ -0,0 +1,25 @@
+#exercicios do capitulo 2 do livro de introdução á programação em python 
+#faca um programa que exiba seu nome na tela
+print("daniely maximo marcon")
+
+
+#escreva um programa que exiba o resultadi de 2ax3b, em que a vale 3 e b vale 5
+a=3
+b=5
+total=(2*a)*(3*b)
+print(total)
+
+#escreva um programa que calcule a soma de tres variaveis e imprima o resultado na tela;
+a=3
+b=4
+c=3
+resultado=a+b+c
+print(resultado)
+
+#modifique o programa 2.2 de forma que ele calcule um aumento de 15% para um salario de 750 reais
+salario=750
+aumento=15
+totalaumento=(salario+(salario*aumento/100))
+print(totalaumento)
+
+
---
+++ semana1/exlivrocap3.py
@@ -0,0 +1,7 @@
+#exercicios do capitulo 3 do livro de introdução á programação em python 
+
+#escreva uma expressão para determinar se uma pessoa deve ou nao pagar imposto. Considere que pagam impostos pessoas cujo salario é maior que 1200.
+salario=1000
+
+situacao=salario>1200
+print(situacao)
